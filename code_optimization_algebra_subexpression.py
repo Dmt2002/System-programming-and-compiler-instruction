93d7c9245cbc0f583ad13b3c0924f49c29024bf2
Digvijay Thakur <92175139+Dmt2002@users.noreply.github.com>
2023-05-09 23:21:26 +0530

    Update code_optimization_algebra_subexpression.py

--- code_optimization_algebra_subexpression.py
+++ code_optimization_algebra_subexpression.py
@@ -1,45 +1,40 @@
-# Code
-class Optimization:
-    def __init__(self, code):
-        self.code = code
-        self.optimized_code = ''
-
-    def algebraic_simplification(self):
-        self.optimized_code = self.code.replace('* 1', '').replace('/ 1', '').replace('+ 0', '').replace('- 0', '')
-        return self.optimized_code
-
-    def common_subexpression_elimination(self):
-        lines = self.optimized_code.split('\n')
-        expressions = {}
-        for i, line in enumerate(lines):
-            if '=' in line:
-                left, right = line.split('=')
-                if right.strip() in expressions:
-                    lines[i] = left + ' = ' + expressions[right.strip()]
-                else:
-                    expressions[right.strip()] = left
-        self.optimized_code = '\n'.join(lines)
-        return self.optimized_code
-
-# Example usage
-code = '''
-x = a + b - b + 1;
-x = a + b - b + 1;
-z = a * 1 / 1 + b * 1;
+code = ''' 
+x = a+1
+y = b*1
+c = j/1
+u = d+e
+v = d+e
 '''
 
-optimizer = Optimization(code)
-optimized_code = optimizer.algebraic_simplification()
-optimized_code = optimizer.common_subexpression_elimination()
-print(optimized_code)
+# Perform algebraic simplification
+new_code = ""
+for line in code.split("\n"):
+    if "=" in line:
+        var, exp = line.split("=")
+        var = var.strip()
+        exp = exp.strip()
+        if "+1" in exp:
+            exp = exp.replace("+1", "")
+        if "*1" in exp:
+            exp = exp.replace("*1", "")
+        if "/1" in exp:
+            exp = exp.replace("/1", "")
+        new_code += var + " = " + exp + ";\n"
+    else:
+        new_code += line + "\n"
 
-# Output
-# x = a + b - b + 1;
-# y = b + 3 - 3 * 1;
-# z = a * 1 / 1 + b * 1;
-# Note
-# This program implements two code optimization techniques: algebraic simplification and common subexpression elimination. The Optimization class has two methods for each technique: algebraic_simplification() and common_subexpression_elimination(). The algebraic_simplification() method simplifies the code by performing algebraic operations such as multiplying by 1 or adding 0, which do not affect the value of the expression. The common_subexpression_elimination() method identifies common subexpressions in the code and replaces them with a variable name.
+# Perform common subexpression elimination
+new_code_lines = new_code.split("\n")
+subexpressions = {}
+for i in range(len(new_code_lines)):
+    line = new_code_lines[i]
+    if "=" in line:
+        var, exp = line.split("=")
+        exp = exp.strip()
+        if exp in subexpressions:
+            new_code_lines[i] = var.strip() + " = " + subexpressions[exp] + ";\n"
+        else:
+            subexpressions[exp] = var.strip()
+new_code = "\n".join(new_code_lines)
 
-# The program takes an input code as a string and creates an instance of the Optimization class. It then calls the algebraic_simplification() method to optimize the code using algebraic simplification and stores the result in optimized_code. Finally, it calls the common_subexpression_elimination() method to optimize the code using common subexpression elimination and stores the result in optimized_code.
-
-# The program then prints out the optimized code. The example code provided demonstrates how the program can be used to optimize a simple code snippet.
+print(new_code)
